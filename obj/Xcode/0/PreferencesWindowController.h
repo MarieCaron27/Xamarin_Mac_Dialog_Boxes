ed0786f2ec2b2bdea86588e51d2e46c229d32209
marie <mariecaron27@gmail.com>
2026-07-07 14:32:18 +0200

    Add dialog boxes Open, Print and Save As

---
+++ obj/Xcode/0/PreferencesWindowController.h
@@ -0,0 +1,18 @@
+// WARNING
+// This file has been generated automatically by Rider IDE to
+// mirror C# types. Changes in this file made by drag-connecting
+// from the UI designer will be synchronized back to C#, but
+// more complex manual changes may not transfer correctly.
+
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
+
+@interface PreferencesWindowController : NSWindowController {
+	NSToolbar *_ToolBar;
+}
+
+@property (nonatomic, retain) IBOutlet NSToolbar *ToolBar;
+
+@end
---
+++ obj/Xcode/1/ViewController.h
@@ -0,0 +1,24 @@
+// WARNING
+// This file has been generated automatically by Rider IDE to
+// mirror C# types. Changes in this file made by drag-connecting
+// from the UI designer will be synchronized back to C#, but
+// more complex manual changes may not transfer correctly.
+
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
+
+@interface ViewController : NSViewController {
+	NSButton *_DialogBtn;
+	NSButton *_SheetBtn;
+	NSTextField *_TextEditor;
+}
+
+@property (nonatomic, retain) IBOutlet NSButton *DialogBtn;
+
+@property (nonatomic, retain) IBOutlet NSButton *SheetBtn;
+
+@property (nonatomic, retain) IBOutlet NSTextField *TextEditor;
+
+@end
