eefc0864481eef9ced9225248c6230894b590dc0
marie <mariecaron27@gmail.com>
2026-07-07 14:31:56 +0200

    Add dialog boxes Open, Print and Save As

--- AppDelegate.cs
+++ AppDelegate.cs
@@ -3,6 +3,9 @@ namespace boites_dialogue
     [Register ("AppDelegate")]
     public class AppDelegate : NSApplicationDelegate
     {
+        public bool ShowPrintAsSheet { get; set;} = true;
+        public bool ShowSaveAsSheet { get; set;} = true;
+        
         #region Computed Properties
         public int NewWindowNumber { get; set;} = -1;
 
@@ -50,7 +53,7 @@ namespace boites_dialogue
             var dlg = NSOpenPanel.OpenPanel;
             dlg.CanChooseFiles = true;
             dlg.CanChooseDirectories = false;
-            dlg.AllowedFileTypes = new string[] { "txt", "html", "md", "css" };
+            dlg.AllowedFileTypes = new string[] { "txt", "html", "md", "css", "cs" };
 
             if (dlg.RunModal() == 1)
             {
@@ -61,7 +64,7 @@ namespace boites_dialogue
                     var path = url.Path;
 
                     var storyboard = NSStoryboard.FromName("Main", null);
-                    var controller = storyboard.InstantiateControllerWithIdentifier("MainWindow") as NSWindowController;
+                    var controller = storyboard.InstantiateInitialController() as NSWindowController;
 
                     controller.ShowWindow(this);
 
@@ -72,6 +75,79 @@ namespace boites_dialogue
                     viewController.View.Window.RepresentedUrl = url;
                 }
             }
+        }
+        
+        [Export ("showPrinter:")]
+        void ShowDocument (NSObject sender) {
+            var dlg = new NSPrintPanel();
+            var storyboard = NSStoryboard.FromName("Main", null);
+            var controller = storyboard.InstantiateInitialController() as NSWindowController;
+
+            // Display the print dialog as dialog box
+            if (ShowPrintAsSheet) {
+                dlg.BeginSheet(new NSPrintInfo(),controller.Window,this,null,new IntPtr());
+            } else {
+                if (dlg.RunModalWithPrintInfo(new NSPrintInfo()) == 1) {
+                    var alert = new NSAlert () {
+                        AlertStyle = NSAlertStyle.Critical,
+                        InformativeText = "We need to print the document here...",
+                        MessageText = "Print Document",
+                    };
+                    alert.RunModal ();
+                }
+            }
+        }
+        
+        [Export ("showLayout:")]
+        void ShowLayout (NSObject sender) {
+            var dlg = new NSPageLayout();
+            var storyboard = NSStoryboard.FromName("Main", null);
+            var controller = storyboard.InstantiateInitialController() as NSWindowController;
+
+            // Display the print dialog as dialog box
+            if (ShowPrintAsSheet) {
+                dlg.BeginSheet (new NSPrintInfo (), controller.Window);
+            } else {
+                if (dlg.RunModal () == 1) {
+                    var alert = new NSAlert () {
+                        AlertStyle = NSAlertStyle.Critical,
+                        InformativeText = "We need to print the document here...",
+                        MessageText = "Print Document",
+                    };
+                    alert.RunModal ();
+                }
+            }
+        }
+        
+        [Export("saveDocumentAs:")]
+        void ShowSaveAs (NSObject sender)
+        {
+            var dlg = new NSSavePanel ();
+            dlg.Title = "Save Text File";
+            dlg.AllowedFileTypes = new string[] { "txt", "html", "md", "css" };
+            var storyboard = NSStoryboard.FromName("Main", null);
+            var controller = storyboard.InstantiateInitialController() as NSWindowController;
+
+            if (ShowSaveAsSheet) {
+                dlg.BeginSheet(controller.Window,(result) => {
+                    var alert = new NSAlert () {
+                        AlertStyle = NSAlertStyle.Critical,
+                        InformativeText = "We need to save the document here...",
+                        MessageText = "Save Document",
+                    };
+                    alert.RunModal ();
+                });
+            } else {
+                if (dlg.RunModal () == 1) {
+                    var alert = new NSAlert () {
+                        AlertStyle = NSAlertStyle.Critical,
+                        InformativeText = "We need to save the document here...",
+                        MessageText = "Save Document",
+                    };
+                    alert.RunModal ();
+                }
+            }
+
         }
     }
 }
